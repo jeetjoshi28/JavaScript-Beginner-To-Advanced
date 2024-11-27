705d10a3e4025ddaba3bda9c9582640d761c5933
jeetjoshi28 <160571864+jeetjoshi28@users.noreply.github.com>
2024-11-27 10:30:04 +0530

    Create objectInJs.js

---
+++ objectInJs.js
@@ -0,0 +1,25 @@
+// what is an object
+
+// Object is are a fundamental part of javascript providing a way to group related data and function togetner. In javascript, an object is collection of key - value pair, where each key is staring and each value can be any data typeof, including other objects. Object can have properies and methods, making them versatile for various use cases.
+
+// SYNTEX
+
+// obj = {}
+
+// creating object
+
+const product = {
+    id: 1,
+    pName: "laptop",
+};
+
+
+const person = {
+    name: "jeet",
+    age: 24,
+    isStudent: true,
+    greet: function () {
+        console.log("Welcome to the person object");
+    },
+};
+
